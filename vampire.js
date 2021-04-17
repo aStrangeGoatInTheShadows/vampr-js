a13b2ce59c4f876b4358ba1d4552902c4db04091
Matt Macdonald <mksmacdonald@gmail.com>
2021-04-17 19:18:06 +0000

    Currently 2 Stretch failing. Presume it requires recursion to parse tree

--- vampire.js
+++ vampire.js
@@ -1,3 +1,5 @@
+const treeLogger = require('tree-logger');
+
 const log = console.log;
 
 class Vampire {
@@ -28,17 +30,20 @@ class Vampire {
 
     while (vamp.creator) {
       vamp = vamp.creator;
-      // log('-------------------------------------------');
-      // log(vamp);
       count++;
     }
 
     return count;
   }
+
+  // Returns true if vampire is offspring of this
+  isOffspring(vampire) {
+    return this.offspring.includes(vampire);
+  }
   
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-    
+    return (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal)    
   }
   
   /** Stretch **/
@@ -49,19 +54,95 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    if (this === vampire) {
+      return this;
+    }
+
+    //// Make oldMan the most senior vampire
+    let oldMan = this;
+    let youngMan = vampire;
+    if (vampire.isMoreSeniorThan(this)){
+      oldMan = vampire;
+      youngMan = this;
+    }
+
+    // if oldman has no creator, he is root so return self
+    if(!oldMan.creator) {
+      return oldMan;
+    }
+    // if young man is old mans child
+    if (oldMan.offspring.includes(youngMan)) {
+      return oldMan;
+    }
     
+    if (youngMan.creator === oldMan.creator) {
+      return youngMan.creator;
+    }
+
+    let parent = oldMan.creator;
+
+    for(let i = parent.numberOfVampiresFromOriginal; i >= 0; i--){
+      log(`${i}th time ran`)
+      log(parent);
+      if(parent.offspring.includes(youngMan)){
+        return parent;
+      }
+      parent = parent.creator;
+    }
+    
+    return null;
   }
 }
 
-//////////////////////////////////////////// WORKING HERE  ////////////////////////////////////
-const ted = new Vampire (`Ted`, 34); // original
-const vlad = new Vampire ('Vlad', 37); // second
-const fred = new Vampire ('Fred', 40);
+//////////////////////////////////// THIS PROBABLY NEEDS RECURSION ///////////////////////////////////////////
+////////////////////////////////////  NEED TO CHECKALL SIBLINGS AND THEIR SIBLINGS FOR THE YOUNG ONE ///////////////////////////////////////////
 
-ted.addOffspring(vlad);
-vlad.addOffspring(fred);
-// Returns the total number of vampires created by that vampire
-log(fred.numberOfVampiresFromOriginal);
+// //////////////////////////////////////////// WORKING HERE  ////////////////////////////////////
+// const ted = new Vampire (`Ted`, 34); // original
+// const vlad = new Vampire ('Vlad', 37); // second
+// const fred = new Vampire ('Fred', 40);
 
-module.exports = Vampire;
+// ted.addOffspring(vlad);
+// vlad.addOffspring(fred);
+// // Returns the total number of vampires created by that vampire
+// //log(ted.closestCommonAncestor(fred));
 
+
+let rootVampire = new Vampire("root");
+
+let offspring1 = new Vampire("1");
+let offspring2 = new Vampire("2");
+let offspring3 = new Vampire("3");
+let offspring4 = new Vampire("4");
+let offspring5 = new Vampire("5");
+let offspring6 = new Vampire("6");
+let offspring7 = new Vampire("7");
+let offspring8 = new Vampire("8");
+
+rootVampire.addOffspring(offspring1);
+rootVampire.addOffspring(offspring2);
+rootVampire.addOffspring(offspring3);
+offspring3.addOffspring(offspring4);
+offspring3.addOffspring(offspring5);
+offspring5.addOffspring(offspring6);
+offspring6.addOffspring(offspring7);
+offspring2.addOffspring(offspring8);
+
+// log(offspring1);
+// log('-------------------------------------------');
+// log(offspring2);
+// log('-------------------------------------------');
+ offspring8.closestCommonAncestor(offspring7);
+
+const root = rootVampire;
+const aTree = treeLogger(root, (node) => node.offspring, (node) => node.name);
+log(aTree);
+
+// it("should be the root vampire for first two offspring", () => {
+  //   expect(offspring1.closestCommonAncestor(offspring2).name).to.equal(rootVampire.name);
+  // })
+  
+  module.exports = Vampire;
+
+  //1) should be offspring 3 for offspring 4 and 7
+  
